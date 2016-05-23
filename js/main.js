4ec82b04a80672be42433764a43fc0b3823fad44
cjrcooper <Coops@Christophers-MacBook-Pro-2.local>
2016-05-23 13:58:40 +1000

    Final

--- js/main.js
+++ js/main.js
@@ -4,11 +4,15 @@ $(document).ready(function(){
   $('.name-tag-line').fadeIn(1000)
   $('.skills, nav').fadeIn(2000);
 
-
-
   var bodyHeight = $('body').height();
   var sectionHeight = bodyHeight/4;
   console.log(sectionHeight)
+
+  $('#scroll').on('click', function(){
+    $("html, body").animate({
+      scrollTop: sectionHeight + 'px'
+    });
+  });
 
 
   $('.about').on('click', function(){
@@ -28,4 +32,40 @@ $(document).ready(function(){
       scrollTop: bodyHeight + 'px'
     });
   })
+});
+
+
+
+
+
+$(document).ready(function(){
+
+
+  var y = function(){
+      TweenMax.to('.st1', 2 , {opacity:1});
+      TweenMax.to('.st3', 2 , {opacity:1});
+      TweenMax.to('.triangle', 2, {opacity:1}).delay(1);
+      TweenMax.to('.thickLine', 2, {opacity:1}).delay(1);
+      TweenMax.to('.main-container', 2, {opacity:1}).delay(1);
+      TweenMax.to('.home, .about, .interests, .contact', 2, {opacity:1}).delay(1);
+
+    };
+
+var x = function(){
+  TweenMax.to('#slashLine', 2 , {rotation:360, onComplete: y})
+};
+
+x();
+
+
+  // var winWidth = $(window).width();
+  //
+  // var resize
+  // if (winWidth <= 480) {
+  //   $('.sml-4').insertAfter('.sml-3');
+  //   $('.sml-3').insertBefore('.sml-5');
+  // } else {
+  //   $('.sml-4').insertBefore('.sml-5');
+  //   $('.sml-3').insertBefore('.about-sml-container-br');
+  // }
 });
